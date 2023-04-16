2b10f77d5f24ab0410ba786a1bdc45fcf0cf2785
ephraim888sun <ephraim888sun@gmail.com>
2023-04-15 20:33:35 -0500

    update pages

--- embedded-client/app/savenote/page.tsx
+++ embedded-client/app/savenote/page.tsx
@@ -5,8 +5,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function SaveNote() {
   return (
-    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
-      <button className='p-4 bg-blue-500 rounded-xl hover:bg-blue-600'>
+    <main className='flex flex-col items-center justify-between min-h-screen p-24 bg-white'>
+      <button className='bg-white hover:bg-gray-200 text-black font-bold py-1 px-2 rounded outline outline-gray-300 font-normal border-1 text-sm'>
         Save Note
       </button>
     </main>
